fc0753616006aa0f61384c9ccd6f726102f94b5c
lsimanic <lorenzo.simanic@gmail.com>
2023-12-09 19:52:14 -0500

    miao

---
+++ src/interrupts.rs
@@ -0,0 +1,33 @@
+use core::arch::asm;
+use crate::pic8259::ChainedPics;
+use spin::Mutex;
+
+pub const PIC_1_OFFSET: u8 = 20;
+pub const PIC_2_OFFSET: u8 = PIC_1_OFFSET + 8;
+
+pub fn enable() {
+    unsafe {
+        asm!("sti", options(preserves_flags, nostack));
+    }
+}
+
+pub static PICS: Mutex<ChainedPics> = Mutex::new(unsafe {
+    ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET)
+});
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum InterruptIndex {
+    Timer = PIC_1_OFFSET,
+    Keyboard,
+}
+
+impl InterruptIndex {
+    fn as_u8(self) -> u8 {
+        self as u8
+    }
+
+    fn as_usize(self) -> usize {
+        usize::from(self.as_u8())
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,29 +1,21 @@
 #![no_std]
 #![no_main]
 
-mod librs;
-mod video_graphics_array;
+mod interrupts;
 mod io;
+mod librs;
+mod pic8259;
+mod video_graphics_array;
 
-use core::panic::PanicInfo;
+use core::panic::PanicInfo; 
 use librs::clear;
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-	clear();
-    println!("Hello World{}", "!");
-    println!(
-        "KFC or {} is an international fast food chain founded in 1930 in North Corbin, Kentucky, USA by Colonel Harland David Sanders.",
-        1 / 3
-    );
-    println!(
-        "The fried chicken restaurant is the second largest fast food chain in the entire world with only McDonald's surpassing it in terms of the number of fast food locations{}",
-        "with 18,875 restaurants in 118 countries and territories as of December 2013."
-    );
-    println!(
-        "In addition to the famous chicken, the restaurant serves a wide variety of foods depending on where in the world it is located such as tacos, ice cream with Jello or fish donuts{}.",
-        22 / 7
-    );
+    clear();
+    println!("Grosse ****");
+	init();
+    println!("****");
     loop {
     }
 }
@@ -31,5 +23,11 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
-    loop {}
+    loop {
+    }
+}
+
+pub fn init() {
+    unsafe { interrupts::PICS.lock().initialize() }
+	interrupts::enable();
 }
