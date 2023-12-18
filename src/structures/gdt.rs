3d37f9a67034ee477fd1d89928da0c09d548ce6d
amuller <amuller@student.42.fr>
2023-12-18 15:50:59 +0100

    gtd define

--- src/structures/gdt.rs
+++ src/structures/gdt.rs
@@ -1,6 +1,17 @@
 use core::arch::asm;
 use lazy_static::lazy_static;
 
+const CODE_KERNEL_EXECUTE_READ: u8 = 0x9a;	// Code, kernel, execute/read
+const CODE_KERNEL_READ: u8 = 0x92;			// Code, kernel, read
+const DATA_READ_WRITE: u8 = 0x96;			// Data, kernel, read/write
+const CODE_KERNEL_EXECUTE: u8 = 0xfa;		// Code, kernel, execute
+const CODE_KERNEL_EXECUTE_READ_CONFORMING: u8 = 0xf2; 	 // Code, kernel, execute/read, conforming
+const CODE_KERNEL_EXECUTE_CONFORMING: u8 = 0xf6;		 // Code, kernel, execute, conforming
+const GRANULARITY4_KB: u8 = 0xc0;  			// Granularity: 4KB
+const SIZE32_BIT: u8 = 0x0f;       			// Default operand and address size: 32-bit
+const MAX_SEGMENT_SIZE: u32 = 0xfffff;		// Maximum segment size
+const NO_OFFSET: u32 = 0;					// No offset
+
 /// Global Descriptor Table entry structure.
 #[repr(C, packed)]
 struct GdtEntry {
@@ -30,12 +41,12 @@ lazy_static! {
 	#[link_section = ".gdt"]
 	static ref GDT: [GdtEntry; 7] = [
 		GdtEntry::new(0, 0, 0, 0),
-		GdtEntry::new(0xfffff, 0, 0x9a, 0xcf),
-		GdtEntry::new(0xfffff, 0, 0x92, 0xcf),
-		GdtEntry::new(0xfffff, 0, 0x96, 0xcf),
-		GdtEntry::new(0xfffff, 0, 0xfa, 0xcf),
-		GdtEntry::new(0xfffff, 0, 0xf2, 0xcf),
-		GdtEntry::new(0xfffff, 0, 0xf6, 0xcf),
+		GdtEntry::new(MAX_SEGMENT_SIZE, NO_OFFSET, CODE_KERNEL_EXECUTE_READ, GRANULARITY4_KB | SIZE32_BIT),
+		GdtEntry::new(MAX_SEGMENT_SIZE, NO_OFFSET, CODE_KERNEL_READ, GRANULARITY4_KB | SIZE32_BIT),
+		GdtEntry::new(MAX_SEGMENT_SIZE, NO_OFFSET, DATA_READ_WRITE, GRANULARITY4_KB | SIZE32_BIT),
+		GdtEntry::new(MAX_SEGMENT_SIZE, NO_OFFSET, CODE_KERNEL_EXECUTE, GRANULARITY4_KB | SIZE32_BIT),
+		GdtEntry::new(MAX_SEGMENT_SIZE, NO_OFFSET, CODE_KERNEL_EXECUTE_READ_CONFORMING, GRANULARITY4_KB | SIZE32_BIT),
+		GdtEntry::new(MAX_SEGMENT_SIZE, NO_OFFSET, CODE_KERNEL_EXECUTE_CONFORMING, GRANULARITY4_KB | SIZE32_BIT),
 	];
 }
 
